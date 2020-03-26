b804fcec8f40166d9e62a8d12056c1d29318a0f4
Pierce Governale <piercegovernale@gmail.com>
2020-03-26 12:49:14 -0400

    RNN upload

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ HMM on the data, and generate a new sonnet which is printed to the console.
 
 import numpy as np
 import nltk
-from data_prep import get_sonnets_from_file, parse_syllable_list, rhyme
+from data_prep import *
 from HMM import HiddenMarkovModel, unsupervised_HMM
+from RNN import *
+
+nltk.download('cmudict')
 
 if __name__ == '__main__':
     # Load the data.
@@ -29,90 +32,40 @@ if __name__ == '__main__':
     # Get the maximum word index is the number of words - 1.
     m = max([max(line) for line in X])
 
+    '''
     # Train the Hidden Markov Model.
     hmm = unsupervised_HMM(X, 20, 1)
 
-    ## Make Shakespeare look like a chump.
+    ## Generate a sonnet using the HMM
     output = []
     last = []
-
-    # Make the first line.
-    lst = hmm.generate_emission(10)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the second line
-    lst = hmm.generate_emission(10)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the third line
-    rhyme_bank = [w2i[word] for word in rhyme(i2w[last[0]], w2i.keys())]
-    lst = hmm.generate_emission(10, rhyme_bank)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the fourth line
-    rhyme_bank = [w2i[word] for word in rhyme(i2w[last[1]], w2i.keys())]
-    lst = hmm.generate_emission(10, rhyme_bank)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the fifth line.
-    lst = hmm.generate_emission(10)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the sixth line
-    lst = hmm.generate_emission(10)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the seventh line
-    rhyme_bank = [w2i[word] for word in rhyme(i2w[last[4]], w2i.keys())]
-    lst = hmm.generate_emission(10, rhyme_bank)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the eight line
-    rhyme_bank = [w2i[word] for word in rhyme(i2w[last[5]], w2i.keys())]
-    lst = hmm.generate_emission(10, rhyme_bank)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the ninth line.
-    lst = hmm.generate_emission(10)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the tenth line
-    lst = hmm.generate_emission(10)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the eleventh line
-    rhyme_bank = [w2i[word] for word in rhyme(i2w[last[8]], w2i.keys())]
-    lst = hmm.generate_emission(10, rhyme_bank)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the twelfth line
-    rhyme_bank = [w2i[word] for word in rhyme(i2w[last[9]], w2i.keys())]
-    lst = hmm.generate_emission(10, rhyme_bank)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the thirteenth line.
-    lst = hmm.generate_emission(10)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
-
-    # Make the fourteenth line
-    rhyme_bank = [w2i[word] for word in rhyme(i2w[last[12]], w2i.keys())]
-    lst = hmm.generate_emission(10, rhyme_bank)[0]
-    output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
-    last.append(lst[0])
+    num_lines = 14
+    rhyme_lines = [3, 4, 7, 8, 11, 12, 14]
+    for i in range(1, num_lines + 1):
+        if i in rhyme_lines:
+            rhyme_bank = [w2i[word] for word in rhyme(i2w[last[i - 3]], w2i.keys())]
+            lst = hmm.generate_emission(10, rhyme_bank)[0]
+        else:
+            lst = hmm.generate_emission(10)[0]
+            
+        output.append(' '.join(map(lambda idx: i2w[idx], reversed(lst))))
+        last.append(lst[0])
 
     ## Present the masterpiece.
     for line in output:
         print(line)
+    '''
+    rnn = setup_rnn(sonnet_list) # Default temperature is 1.0 -> currently not even implemented
+    temperatures = [1.5, 0.75, 0.25]
+    
+    for temp in temperatures:
+        print("\nTEMPERATURE = ", temp)
+        print('#####################')
+        sequence = "shall i compare thee to a summer's day?\n"
+        print(sequence, end='')
+        
+        for i in range(200): # generate 200 characters
+            res = rnn.predict(sequence, temperature=temp)
+            print(res, end='')
+            sequence += res
+            sequence = sequence[1:]
